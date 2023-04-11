b2d5fb187b6721936bd6ee6c9dd52cf73fa2f28e
Mo Abdo <moabdo1212@gmail.com>
2023-04-11 12:53:43 -0400

    updated directory component, moved category item and directory into separate components

---
+++ src/components/directory/directory.jsx
@@ -0,0 +1,14 @@
+import './directory.scss';
+import CategoryItem from '../categoryItem/category-item';
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
